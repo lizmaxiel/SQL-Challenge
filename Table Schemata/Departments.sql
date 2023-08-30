c942586745941946150a873636ab2ceb3a9f6c61
lizmaxiel <135934423+lizmaxiel@users.noreply.github.com>
2023-08-29 21:57:46 -0400

    update

---
+++ Table Schemata/Departments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    department_name VARCHAR
+);
+
+SELECT *
+FROM Departments
+
---
+++ Table Schemata/Employees.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Employees (
+	emp_no INTEGER,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE,
+	CONSTRAINT pk_Employees PRIMARY KEY (emp_no)
+);
+
+SELECT *
+FROM Employees
+
+
+
+
+	
